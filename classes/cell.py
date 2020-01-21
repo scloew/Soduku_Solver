f8959ca3dd95c1410483f623bb0eacac70af999e
scloew <stevecloew@gmail.com>
2020-01-20 22:06:38 -0600

    Changed cell to take group instad of list of cells. Changed temp_driver to test against groups. Found two issues - 1) Square group not updating. 2) Square group update seems to be changing puzzle cells structure... somehow?????

--- classes/cell.py
+++ classes/cell.py
@@ -5,11 +5,10 @@ class Cell:
         self.options = set() if self.val == 0 else {str(i) for i in range(1, 10)} #TODO is the == 0 necessary?
 
     def update_options(self, group):
-        self.options -= {c.val for c in group if c.val}
+        self.options = self.options.intersection(group.options)
         if len(self.options) == 1:
             self.val = self.options.pop()
             self.options = set()
             return True
-        print(f'cell value = {self.val}')
         return False #TODO don't love duplicate return statement in this case
                      #TODO do I want puzzle to check if cell needs to be updated -> think so
--- temp_driver.py
+++ temp_driver.py
@@ -1,38 +1,44 @@
 from classes.puzzle import Puzzle
 
-#file = r'test_input\test_2.txt'
-#file = r'test_input\test_update_cell_val_invalidPuzzle.txt' #Note this is a puzzle used to check cell will take value when only 1 option
+print('\n*****\nChecking against row\n*****\n')
 file = r'test_input\test_row_update.txt'
 p = Puzzle(file)
+print('original puzzle\n')
 p.print_puzzle()
+print()
 cell = p.cells[0][0]
-print(f'test group 1.row = {[c.val for c in p.cells[0]]}')
-cell.update_options(p.cells[0])
-new_group = [p.cells[i][0] for i in range(9)]
-print(f'test group 1.column = {[c.val for c in new_group]}')
+print(f'test group 1.row = {[c.val for c in p.rows[0].cells]}\n')
+cell.update_options(p.rows[0])
+new_group = p.columns[0]
+print(f'test group 1.column = {[c.val for c in new_group.cells]}\n')
 cell.update_options(new_group)
+print('updated puzzle\n')
 p.print_puzzle()
 
 print('\n*****\nChecking against column\n*****\n')
 file = r'test_input\test_column_update.txt'
 p_new = Puzzle(file)
+print('original puzzle\n')
 p_new.print_puzzle()
+print()
 cell = p_new.cells[0][0]
-print(f'test group 2.row = {[c.val for c in p_new.cells[0]]}')
-cell.update_options(p_new.cells[0])
-new_group = [p_new.cells[i][0] for i in range(9)]
-print(f'test group 2.column = {[c.val for c in new_group]}') #TODO this group failing
+print(f'test group 2.row = {[c.val for c in p_new.rows[0].cells]}\n')
+cell.update_options(p_new.rows[0])
+new_group = p_new.columns[0]
+print(f'test group 2.column = {[c.val for c in new_group.cells]}\n')
 cell.update_options(new_group)
+print('updated puzzle\n')
 p_new.print_puzzle()
 
-print('\n*****\nChecking against column\n*****\n')
-file = r'test_input\test_building_squares.txt'
+print('\n*****\nChecking against squares\n*****\n')
+file = r'test_input\test_square_update.txt'
 p_newer = Puzzle(file)
+print()
+print('original puzzle\n')
 p_newer.print_puzzle()
 cell = p_newer.cells[0][0]
-print(f'test group 2.row = {[c.val for c in p_newer.cells[0]]}')
-cell.update_options(p_newer.cells[0])
-new_group = [p_newer.cells[i][0] for i in range(9)]
-print(f'test group 2.column = {[c.val for c in new_group]}') #TODO this group failing
+new_group = p_newer.squares[0]
+print(f'test group 3.square = {[c.val for c in new_group.cells]}') #TODO this group failing
 cell.update_options(new_group)
-p_newer.print_puzzle()
+print('updated puzzle\n')
+p_newer.print_puzzle() #TODO ummm..... why is puzzle changin??????
